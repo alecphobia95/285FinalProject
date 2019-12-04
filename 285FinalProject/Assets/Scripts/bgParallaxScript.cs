9b7f0575dc7861ebf0ccf2594bb822a9f68416d4
PisceanSquid <55410838+PisceanSquid@users.noreply.github.com>
2019-12-04 15:58:11 -0500

    So...it has begun (UI :) )
    Added Necessary UI
    
    Need to add:
    -FX for button clicks
    -Scripts for UI functions
    -How to Panel

---
+++ 285FinalProject/Assets/Scripts/bgParallaxScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgParallaxScript : MonoBehaviour
+{
+    public GameObject planeGameObject;
+    private Renderer planeRenderer;
+
+    public float scrollSpeed;
+
+    void Start()
+    {
+        planeRenderer = planeGameObject.GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        Vector2 textureOffset = new Vector2(Time.time * scrollSpeed, 0);
+        planeRenderer.material.mainTextureOffset = textureOffset;
+    }
+}
+
